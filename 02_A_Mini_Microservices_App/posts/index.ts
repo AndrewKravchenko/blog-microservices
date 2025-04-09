ee129258971322f609a181857624f8954cd8c691
Andrey <andreikrauchanka1994@gmail.com>
2025-04-09 14:21:43 +0300

    feat(posts): add posts service

---
+++ 02_A_Mini_Microservices_App/posts/index.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { randomBytes } from 'crypto';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+type Post = {
+  id: string;
+  title: string;
+}
+
+const POSTS_ROUTE = '/posts';
+const posts: Record<string, Post> = {};
+
+const generatePostId = (): string => randomBytes(4).toString('hex');
+
+app.get(POSTS_ROUTE, (req: Request, res: Response) => {
+  res.send(posts);
+});
+
+app.post(POSTS_ROUTE, async (req: Request, res: Response) => {
+  const id = generatePostId();
+  const { title } = req.body;
+
+  if (!title) {
+    res.status(400).send({ error: 'Title is required' });
+  }
+
+  const newPost: Post = { id, title };
+  posts[id] = newPost;
+
+  res.status(201).send(newPost);
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
